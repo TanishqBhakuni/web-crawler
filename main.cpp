a42491a31a2e8a30da7fc41bba827d5aa1dd379f
TanishqBhakuni <bhatan19972fam@gmail.com>
2025-09-14 23:18:49 +0530

    first commit

--- main.cpp
+++ main.cpp
@@ -3,15 +3,28 @@
 
 int main() {
 
-    std::string target_url = "https://indianexpress.com/article/business/companies/infosys-buyback-shareholders-benefit-10247117/";
+    std::string target_url = "https://example.com";
     std::cout << "Attempting to fetch URL: " << target_url << std::endl;
 
     cpr::Response response = cpr::Get(cpr::Url{target_url});
 
     if(response.status_code == 200){
-        std::cout << "Successfully fetched the URL"<<response.status_code << std::endl;
+        
+        std::cout << "Request successful! Status code: " << response.status_code << std::endl;
+        
+        std::cout << "--- Begin HTML Content ---" << std::endl;
+
+        std::cout << response.text << std::endl;
+
+        std::cout << "--- End HTML Content ---" << std::endl;
     }else{
-        std::cout << "Failed to fetch the URL"<<response.status_code << std::endl;
+
+        std::cout << "Failed to fetch the URL"<< response.status_code << std::endl;
     }
+
+    if(response.error){
+        std::cout << "Error: " << response.error.message << std::endl;
+    }
+
     return 0;
 }
