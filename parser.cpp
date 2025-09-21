316e06a90dbbe6cee88950f36f3b1747769c33bc
TanishqBhakuni <bhatan19972fam@gmail.com>
2025-09-21 19:07:52 +0530

    commit

--- parser.cpp
+++ parser.cpp
@@ -6,19 +6,28 @@ static void search_for_links(GumboNode* node, std::vector<std::string>& links){
     if (node -> type != GUMBO_NODE_ELEMENT){
         return;
     }
+
+    GumboElement& element = node->v.element;
+
+    for(unsigned int i=0; i<element.children.length; ++i){
+        search_for_links(static_cast<GumboNode*>(element.children.data[i]), links);
+    }
 }
 
 std::vector<std::string> find_links(const std::string& html_body, const std::string& base_url){
     GumboOutput* output = gumbo_parse(html_body.c_str());
 
     if(output -> status != GUMBO_STATUS_OK){
-        std::cerr << "Failed to parse HTML" << std::endl;
+        std::cerr << "Error: Failed to parse HTML" << std::endl;
         gumbo_destroy_output(&kGumboDefaultOptions, output);
         return {};
     }
 
+    std::vector<std::string> links;
+    search_for_links(output -> root, links);
+
     std::cout<<"HTML parsing successful. Root node type: " << output -> root -> type << std::endl;
 
     gumbo_destroy_output(&kGumboDefaultOptions, output);
-    return {};
+    return links;
 }
