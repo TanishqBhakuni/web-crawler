78f2e37e75f5f4fc7f2cd56f026ca43c467842be
TanishqBhakuni <bhatan19972fam@gmail.com>
2025-09-26 23:51:04 +0530

    Refactor ThreadSafeQueue class: Add missing include directives for queue, mutex, and condition_variable.

--- thread_safe_structures.hpp
+++ thread_safe_structures.hpp
@@ -1,5 +1,18 @@
 #ifndef THREAD_SAFE_STRUCTURES_HPP
 #define THREAD_SAFE_STRUCTURES_HPP
 
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+template <typename T>
+class ThreadSafeQueue{
+    public:
+
+    private:
+    mutable std::mutex mtx;
+    std::queue<T> data_queue;
+    std::condition_variable cv;
+};
 
 #endif 
